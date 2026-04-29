4abca5ad5b4a4a62dcf5b93bd5cc2ea2e86237e4
Alessio <alexdrastico616@gmail.com>
2026-04-29 20:50:45 +0200

    Add files via upload

---
+++ app source code/app/build/generated/source/buildConfig/debug/com/example/luna/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.luna;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.luna";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final String GEMINI_API_KEY = "AIzaSyATgH7hEsQxbp985disfcu-zBQu5Nl8YOU";
+}
